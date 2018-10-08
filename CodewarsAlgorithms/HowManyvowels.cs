9c9c23a92661d8d557b1e6d045af3bc5b47e98c6
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-08 10:02:06 +0100

    Initial commit

---
+++ CodewarsAlgorithms/HowManyvowels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsAlgorithms
+{
+
+        public class Kata
+        {
+            public static int GetVowelCount(string str)
+            {
+                int vowelCount = 0;
+
+                // Your code here
+                char[] letters = str.ToLower().ToCharArray();
+
+                foreach (char c in letters)
+                {
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        vowelCount++;
+                    }
+                }
+
+                return vowelCount;
+            }
+        }
+
+    }
--- CodewarsAlgorithms/Program.cs
+++ CodewarsAlgorithms/Program.cs
@@ -10,8 +10,7 @@ namespace CodewarsAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            
         }
     }
 }
