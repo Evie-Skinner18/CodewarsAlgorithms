eb4304600b621b20ba69f4aa06fa0f708ec6dcac
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-08 13:35:34 +0100

    Trying to write a GetMiddlecharacter algorithm

---
+++ CodewarsAlgorithms/GetTheMiddleCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsAlgorithms
+{
+    class GetTheMiddleCharacter
+    {
+        public string GetMiddle(string s)
+        {
+            string middle = "";
+            var letters = s.ToCharArray();
+            for(int i = 0; i < letters.Length; i++)
+            {
+                letters.Length % 2 == 0? middle = 
+                    // if odd, rtn the char at index letters.Length / 2.
+            }
+    }
+}
