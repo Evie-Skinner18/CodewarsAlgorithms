9ff74dd9b0a414fb15c76084046748ff3bcba3f2
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-02-10 18:47:08 +0000

    Started a new challenge: 6kyu highest scoring word

---
+++ C#/CodewarsAlgorithms/HighestScoringWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CodewarsAlgorithms
+{
+    public class HighestScoringWord
+    {
+        public static string CalculateHighestScoringWord(string s)
+        {
+            var listOfWords = s.Substring(" ").ToList();
+            var score = 0;
+
+            foreach (var word in listOfWords)
+            {
+                //var listOfChars = word.ToList<char>();
+                score = word.Contains('a') ? score + 1 : score;
+
+
+            }
+
+        }
+    }
+}
