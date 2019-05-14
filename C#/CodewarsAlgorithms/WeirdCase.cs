1d19cfeb2ff3dc31bc996151e9f006eeb478cb87
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-14 16:35:37 +0100

    The strings Im making are definitely weird but not weird in the right way

--- C#/CodewarsAlgorithms/WeirdCase.cs
+++ C#/CodewarsAlgorithms/WeirdCase.cs
@@ -11,16 +11,26 @@ public class WeirdCase
         var stringList = s.ToList();
         var stringBuilder = new StringBuilder();
         var charsOfThisIndex = new List<char>();
+        var numberOfAppearances = 0;
         
 
         foreach (var c in stringList)
         {
-            charsOfThisIndex.Add(stringList[stringList.IndexOf(c)]);
-
-            foreach (var character in charsOfThisIndex)
+            //charsOfThisIndex.Add(stringList[stringList.IndexOf(c)]);
+            charsOfThisIndex = stringList.FindAll(character => character.Equals(c));
+            numberOfAppearances = charsOfThisIndex.Count();
+            if (numberOfAppearances > 1)
             {
-                stringBuilder.Append(charsOfThisIndex.IndexOf(character) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+                foreach (var charOfThisIndex in charsOfThisIndex)
+                {
+                    stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+
+                }
             }
+            
+
+            stringBuilder.Append(stringList.IndexOf(c) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+
         }
 
         return stringBuilder.ToString();
