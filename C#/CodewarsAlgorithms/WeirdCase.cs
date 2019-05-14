1609a8576397605371e8db26beefb7b0ec887001
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-14 18:34:57 +0100

    Found a complicated Linq query on stack overflow but I dont fully understand it yet so not helping

--- C#/CodewarsAlgorithms/WeirdCase.cs
+++ C#/CodewarsAlgorithms/WeirdCase.cs
@@ -12,24 +12,56 @@ public class WeirdCase
         var stringBuilder = new StringBuilder();
         var charsOfThisIndex = new List<char>();
         var numberOfAppearances = 0;
+        var indicesOfThisChar = new List<int>();
         
 
-        foreach (var c in stringList)
+        foreach (var ch in stringList)
         {
             //charsOfThisIndex.Add(stringList[stringList.IndexOf(c)]);
-            charsOfThisIndex = stringList.FindAll(character => character.Equals(c));
+            charsOfThisIndex = stringList.FindAll(character => character.Equals(ch));
             numberOfAppearances = charsOfThisIndex.Count();
+
             if (numberOfAppearances > 1)
             {
-                foreach (var charOfThisIndex in charsOfThisIndex)
-                {
-                    stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+                var n = stringList.IndexOf(ch);
+                var result = stringList
+                    .Select((c, i) => new { c, i })
+                    .Where(x => x.c == ch)
+                    .Skip(1)
+                    .FirstOrDefault();
 
-                }
+
+                 //return result != null ? result.i : -1;
+                stringBuilder.Append(result.i % 2 == 0 ? result.c.ToString().ToUpper() : result.c.ToString().ToLower());
             }
-            
+            //indicesOfThisChar.Add(c);
 
-            stringBuilder.Append(stringList.IndexOf(c) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+            //if (numberOfAppearances > 1)
+            //{
+            //    foreach (var charOfThisIndex in charsOfThisIndex)
+            //    {
+            //        //stringBuilder.Append(stringList.IndexOf(charOfThisIndex,
+            //        //    charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? charOfThisIndex.ToString().ToUpper() : charOfThisIndex.ToString().ToLower()));
+
+                   
+            //        //stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+
+            //    }
+            //}
+
+           // stringBuilder.Append(stringList.IndexOf(c) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+
+            //if (numberOfAppearances > 1)
+            //{
+            //    foreach (var charOfThisIndex in charsOfThisIndex)
+            //    {
+            //        stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
+
+            //    }
+            //}
+
+
+            stringBuilder.Append(stringList.IndexOf(ch) % 2 == 0 ? ch.ToString().ToUpper() : ch.ToString().ToLower());
 
         }
 
