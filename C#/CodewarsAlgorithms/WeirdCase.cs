f82c08295002b6ab41d8b83be54c5efcd2b3e3bf
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-15 17:12:46 +0100

    Found a different LINQ idea on stack overflow that is new to me but very much understandable

--- C#/CodewarsAlgorithms/WeirdCase.cs
+++ C#/CodewarsAlgorithms/WeirdCase.cs
@@ -13,27 +13,40 @@ public class WeirdCase
         var charsOfThisIndex = new List<char>();
         var numberOfAppearances = 0;
         var indicesOfThisChar = new List<int>();
-        
-
-        //foreach (var ch in stringList)
-        //{
-        //    //charsOfThisIndex.Add(stringList[stringList.IndexOf(c)]);
-        //    charsOfThisIndex = stringList.FindAll(character => character.Equals(ch));
-        //    numberOfAppearances = charsOfThisIndex.Count();
-
-        //    if (numberOfAppearances > 1)
-        //    {
-        //        var n = stringList.IndexOf(ch);
-        //        var result = stringList
-        //            .Select((c, i) => new { c, i })
-        //            .Where(x => x.c == ch)
-        //            .Skip(1)
-        //            .FirstOrDefault();
 
 
-        //         //return result != null ? result.i : -1;
-        //        stringBuilder.Append(result.i % 2 == 0 ? result.c.ToString().ToUpper() : result.c.ToString().ToLower());
-        //    }
+        foreach (var ch in stringList)
+        {
+            //charsOfThisIndex.Add(stringList[stringList.IndexOf(c)]);
+            charsOfThisIndex = stringList.FindAll(character => character.Equals(ch));
+            numberOfAppearances = charsOfThisIndex.Count();
+
+            if (numberOfAppearances > 1)
+            {
+                var duplicates = stringList.Select((t, i) => new { Index = i, Text = t })
+                    .GroupBy(g => g.Text).Where(g => g.Count() > 1);
+
+                foreach (var letterGroup in duplicates)
+                {
+                    foreach (var letter in letterGroup)
+                    {
+                        stringBuilder.Append(letter.Index % 2 == 0 ? letter.Text.ToString().ToUpper() : letter.Text.ToString().ToLower());
+                    }
+                }
+            }
+
+            stringBuilder.Append(stringList.IndexOf(ch) % 2 == 0 ? ch.ToString().ToUpper() : ch.ToString().ToLower());
+
+
+            //var result = stringList
+            //    .Select((c, i) => new { c, i })
+            //    .Where(x => x.c == ch)
+            //    .Skip(1)
+            //    .FirstOrDefault();
+            //    }
+
+            //return result != null ? result.i : -1;
+            //        
             //indicesOfThisChar.Add(c);
 
             //if (numberOfAppearances > 1)
@@ -43,26 +56,27 @@ public class WeirdCase
             //        //stringBuilder.Append(stringList.IndexOf(charOfThisIndex,
             //        //    charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? charOfThisIndex.ToString().ToUpper() : charOfThisIndex.ToString().ToLower()));
 
-                   
+
             //        //stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
 
             //    }
             //}
 
-           // stringBuilder.Append(stringList.IndexOf(c) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
 
             //if (numberOfAppearances > 1)
             //{
             //    foreach (var charOfThisIndex in charsOfThisIndex)
             //    {
-            //        stringBuilder.Append(charsOfThisIndex.IndexOf(charOfThisIndex) % 2 == 0 ? c.ToString().ToUpper() : c.ToString().ToLower());
 
             //    }
             //}
 
 
 
-        //}
+
+            //}
+
+        }
 
         return stringBuilder.ToString();
     }
